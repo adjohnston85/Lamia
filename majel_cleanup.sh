3ebf2e5a37db31245c5bfd428d7d667d29e44e60
Andrew Johnston <joh592@pearcey-login.cm.hpc>
2021-09-06 21:39:42 +1000

    added removal of fastq files to cleanup

--- majel_cleanup.sh
+++ majel_cleanup.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
+if [ -z $1 ]
+then
+    SAMPLE_NAME=$(basename $PWD)
+else
+    SAMPLE_NAME=$1
+fi
+
+rm -r ./data
+rm *.fastq.gz
 rm *.fq.gz
-rm $1_s.b*
-rm $1.bam
+rm ${SAMPLE_NAME}_s.b*
+rm ${SAMPLE_NAME}.bam
 mkdir stats
 mkdir ./stats/trim_galore
 mv *trimming_report* ./stats/trim_galore/
-mv $1*txt ./stats/
+mv ${SAMPLE_NAME}*txt ./stats/
 mkdir ./stats/methyldackyl
-mv $1*svg ./stats/methyldackyl/
+mv ${SAMPLE_NAME}*svg ./stats/methyldackyl/
 mkdir ./stats/picard
-mv $1*metrics.test ./stats/picard/
+mv ${SAMPLE_NAME}*metrics.test ./stats/picard/
 mkdir QC
 mv *fastqc* ./QC/
 rm -r ./tmp
 mkdir browser_tracks
-mv $1_sd_CpG.tdf ./browser_tracks/
+mv ${SAMPLE_NAME}_sd_CpG.tdf ./browser_tracks/
 mkdir MethylSeekR
-if grep -q "MethylSeekR not run, coverage < 10x" $1_PMD.bed
+if grep -q "MethylSeekR not run, coverage < 10x" ${SAMPLE_NAME}_PMD.bed
 then
   echo "MethylSeekR not run, inadequate coverage"
   touch ./MethylSeekR/InadequateCoverage.txt
-  rm $1{_PMD.bed,_UMRLMR.bed,_wPMD_UMRLMR.bed}
+  rm ${SAMPLE_NAME}_PMD.bed,_UMRLMR.bed,_wPMD_UMRLMR.bed
 else
   mv *UMR* ./MethylSeekR/
   mv *PMD* ./MethylSeekR/
   mv *CalculateFDR* ./MethylSeekR/
   mv *Segmentation* ./MethylSeekR/
-  mv $1_AlphaDistribution.pdf ./MethylSeekR/
+  mv ${SAMPLE_NAME}_AlphaDistribution.pdf ./MethylSeekR/
 fi
 echo 'Compressing all text files'
 find . \( -name "*.txt" -o -name "*.bedGraph" -o -name "*.bed" \) -exec gzip {} \;
